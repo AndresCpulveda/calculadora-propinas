de1c08e2a7e860ada22e86efba0d177f056261a0
Andres Sepulveda <andresfelipesepulveda@live.com>
2022-08-19 08:23:02 -0500

    Js code finished

--- js/app.js
+++ js/app.js
@@ -60,7 +60,6 @@ function consultarPlatillos() {
 }
 
 function mostrarPlatillos(datos) {
-  console.log(datos);
   const contenido = document.querySelector('#platillos .contenido')
   datos.forEach( cur => {
     const row = document.createElement('div');
@@ -122,9 +121,279 @@ function editarCantidad(platillo, cantidad) {
     const filtrado = objCliente.pedido.filter(ele => ele.id != id)
     objCliente.pedido = filtrado;
   }
-  actualizarResumen();
+
+  if(objCliente.pedido.length == 0) {
+    mensajePedidoVacio();
+  }else {
+    actualizarResumen();
+  }
+
 }
 
 function actualizarResumen() {
+  limpiarHtml()
+  const contenido = document.querySelector('#resumen .contenido');
+  const resumen = document.createElement('div');
+  resumen.classList.add('col-md-6', 'card', 'py-2', 'px-3', 'shadow')
+
+  //Info de la mesa
+  const mesa = document.createElement('p')
+  mesa.textContent = `Mesa: `
+  mesa.classList.add('fw-bold')
   
+  const mesaSpan = document.createElement('span');
+  mesaSpan.textContent = objCliente.mesa;
+  mesaSpan.classList.add('fw-normal');
+  mesa.append(mesaSpan);
+
+  //Info de la hora
+  const hora = document.createElement('p')
+  hora.textContent = `Mesa: `
+  hora.classList.add('fw-bold')
+  
+  const horaSpan = document.createElement('span');
+  horaSpan.textContent = objCliente.hora;
+  horaSpan.classList.add('fw-normal');  
+  hora.append(horaSpan);
+
+  // Titulo de la sección
+  const heading = document.createElement('h3');
+  heading.textContent = 'Platillos Consumidos';
+  heading.classList.add('my-4', 'text-center');
+
+  //Iterar sobre el array de pedidos
+  const grupo = document.createElement('ul');
+  grupo.classList.add('list-group');
+
+  const {pedido} = objCliente;
+  pedido.forEach( cur => {
+    const {nombre, cantidad, precio, id} = cur;
+
+    const lista = document.createElement('li');
+    lista.classList.add('list-group-item');
+
+    const nombreTexto = document.createElement('h4');
+    nombreTexto.classList.add('my-4')
+    nombreTexto.textContent = nombre;
+
+    // Cantidad del articulo
+    const cantidadTexto = document.createElement('p');
+    cantidadTexto.classList.add('fw-bold');
+    cantidadTexto.textContent = 'Cantidad: '
+
+    const cantidadValor = document.createElement('span');
+    cantidadValor.classList.add('fw-normal');
+    cantidadValor.textContent = cantidad;
+
+    cantidadTexto.append(cantidadValor);
+
+    //Precio
+    const precioTexto = document.createElement('p');
+    precioTexto.classList.add('fw-bold');
+    precioTexto.textContent = 'Precio: '
+
+    const precioValor = document.createElement('span');
+    precioValor.classList.add('fw-normal');
+    precioValor.textContent = `$${precio}`;
+
+    precioTexto.append(precioValor);
+    
+    //Sub-total
+    const subtotalTexto = document.createElement('p');
+    subtotalTexto.classList.add('fw-bold');
+    subtotalTexto.textContent = 'Sub-total: '
+
+    const subtotalValor = document.createElement('span');
+    subtotalValor.classList.add('fw-normal');
+    subtotalValor.textContent = `$${precio * cantidad}`;
+    
+    // Boton para eliminar
+    const btnEliminar = document.createElement('button');
+    btnEliminar.classList.add('btn', 'btn-danger');
+    btnEliminar.textContent = 'Eliminar del Pedido';
+    
+    //Funcion para eliminar del pedido
+    btnEliminar.onclick = function() {
+      eliminarProducto(id)
+    }
+
+
+    subtotalTexto.append(subtotalValor);
+    
+    //Agregar elementos al li
+    lista.append(nombreTexto, cantidadTexto, precioTexto, subtotalTexto, btnEliminar)
+    grupo.append(lista)
+  })
+  
+  //Agregar al contenido
+  resumen.append(heading, mesa, hora, grupo);
+  contenido.append(resumen)
+  mostrarFormPropinas();
+}
+
+function limpiarHtml() {
+  const contenido = document.querySelector('#resumen .contenido');
+  while(contenido.firstChild) {
+    contenido.removeChild(contenido.firstChild)
+  }
+}
+
+function eliminarProducto(id) {
+  const filtrado = objCliente.pedido.filter(ele => ele.id !== id)
+  objCliente.pedido = filtrado;
+
+  const borrado = document.querySelector(`#producto-${id}`)
+  borrado.value = 0
+
+  if(objCliente.pedido.length == 0) {
+    mensajePedidoVacio();
+  }else {
+    actualizarResumen();
+  }
+}
+
+function mensajePedidoVacio() {
+  limpiarHtml()
+  const contenido = document.querySelector('#resumen .contenido');
+
+  const texto = document.createElement('p')
+  texto.classList.add('text-center')
+  texto.textContent = 'Añade los elementos del pedido'
+
+  contenido.append(texto)
+}
+
+function mostrarFormPropinas() {
+  const contenido = document.querySelector('#resumen .contenido');
+
+  const formulario = document.createElement('div')
+  formulario.classList.add('col-md-6', 'formulario')
+
+  const divForm = document.createElement('div')
+  divForm.classList.add('card', 'py-2', 'px-3', 'shadow')
+
+  const heading = document.createElement('h3')
+  heading.classList.add('my-4', 'text-center')
+  heading.textContent = 'Propina'
+
+  //Radio button 10%
+  const radio10 = document.createElement('input')
+  radio10.type = 'radio'
+  radio10.name = 'propina'
+  radio10.value = 10
+  radio10.classList.add('form-check-input')
+  radio10.onclick = function() {
+    calcularPropina(radio10.value)
+  }
+
+  const radio10Label = document.createElement('label')
+  radio10Label.textContent = '10%'
+  radio10Label.classList.add('form-check-label')
+
+  const radio10Div = document.createElement('div')
+  radio10Div.classList.add('form-check')
+
+  radio10Div.append(radio10, radio10Label)
+  
+  //Radio button 20%
+  const radio20 = document.createElement('input')
+  radio20.type = 'radio'
+  radio20.name = 'propina'
+  radio20.value = 20
+  radio20.classList.add('form-check-input')
+  radio20.onclick = function() {
+    calcularPropina(radio20.value)
+  }
+
+  const radio20Label = document.createElement('label')
+  radio20Label.textContent = '20%'
+  radio20Label.classList.add('form-check-label')
+
+  const radio20Div = document.createElement('div')
+  radio20Div.classList.add('form-check')
+
+  radio20Div.append(radio20, radio20Label)
+
+  //Radio button 25%
+  const radio25 = document.createElement('input')
+  radio25.type = 'radio'
+  radio25.name = 'propina'
+  radio25.value = 25
+  radio25.classList.add('form-check-input')
+  radio25.onclick = function() {
+    calcularPropina(radio25.value)
+  }
+
+  const radio25Label = document.createElement('label')
+  radio25Label.textContent = '25%'
+  radio25Label.classList.add('form-check-label')
+
+  const radio25Div = document.createElement('div')
+  radio25Div.classList.add('form-check')
+
+  radio25Div.append(radio25, radio25Label)
+
+
+  divForm.append(heading, radio10Div, radio20Div, radio25Div)
+  formulario.append(divForm)
+  contenido.append(formulario)
+  
+}
+
+function calcularPropina(porcentaje) {
+  const {pedido} = objCliente;
+  let subtotal = 0;
+  pedido.forEach( cur => {
+    subtotal += cur.precio * cur.cantidad
+  })
+  const propina = parseInt(subtotal * (porcentaje / 100))
+  const total = parseInt(subtotal * (porcentaje / 100 + 1))
+
+  mostrarTotal(subtotal, total, propina)
+}
+
+function mostrarTotal(subtotal, total, propina) {
+
+  const divTotales = document.createElement('div')
+  divTotales.id = 'container-totales'
+
+  const subtotalText = document.createElement('p')
+  subtotalText.classList.add('fs-4', 'fw-bold', 'mt-2')
+  subtotalText.textContent = `Subtotal Consumo: `
+
+  const subtotalSpan = document.createElement('span')
+  subtotalSpan.classList.add('fw-normal')
+  subtotalSpan.textContent = `$${subtotal}`
+
+  subtotalText.append(subtotalSpan);
+
+  const propinaText = document.createElement('p')
+  propinaText.classList.add('fs-4', 'fw-bold', 'mt-2')
+  propinaText.textContent = `Propina: `
+
+  const propinaSpan = document.createElement('span')
+  propinaSpan.classList.add('fw-normal')
+  propinaSpan.textContent = `$${propina}`
+
+  propinaText.append(propinaSpan);
+
+  const totalText = document.createElement('p')
+  totalText.classList.add('fs-4', 'fw-bold', 'mt-2')
+  totalText.textContent = `Total: `
+
+  const totalSpan = document.createElement('span')
+  totalSpan.classList.add('fw-normal')
+  totalSpan.textContent = `$${total}`
+
+  totalText.append(totalSpan);
+
+  const containerTotales = document.querySelector('#container-totales')
+  if(containerTotales) {
+    containerTotales.remove()
+  }
+
+  divTotales.append(subtotalText, propinaText, totalText)
+
+  const formulario = document.querySelector('.formulario > div')
+  formulario.append(divTotales)
 }
